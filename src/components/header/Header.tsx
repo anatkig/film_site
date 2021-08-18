3dddb437537fbdd024abb157289eb3706ec4b8b4
Anatoliy Kiguradze <keeggooggle@gmail.com>
2021-08-18 09:25:43 +0600

    worked of input style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useGetFilmByTitleQuery } from "../../redux/slices/apiSlice";
+import { useHistory } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [filmTitle, setFilmTitle] = useState<string>("");
+  const history = useHistory();
+
+  const { data, error, isLoading } = useGetFilmByTitleQuery(filmTitle);
+  console.log(data);
+  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === "Enter") {
+      setFilmTitle(inputValue);
+      if (!error) {
+        history.push("/film_page");
+      }
+    }
+  };
   return (
     <>
       <h1>Film World</h1>
-      <div className="miniForm">
+      <div className="mini_form">
         <Link to="/">
-          <button>Home</button>
+          <button className="home_button">Home</button>
         </Link>
-        <input className="input" placeholder="search for a film..." />
+        <div className="input_container">
+          <input
+            className="input"
+            value={inputValue}
+            placeholder="search for a film..."
+            onChange={(event) => setInputValue(event.target.value)}
+            onKeyUp={handleSearch}
+          />
+          <button className="search_button">
+            <SearchIcon />
+          </button>
+        </div>
       </div>
     </>
   );
